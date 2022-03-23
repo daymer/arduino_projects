f625d0560b8f931d69158b5707b187131b074d88
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2022-03-23 15:57:37 +0300

    minor edits

--- sketch_test_1/sketch_test_1.ino
+++ sketch_test_1/sketch_test_1.ino
@@ -4,20 +4,17 @@
 
 #define driveONEstepsPerRota 1600 // steps per 1 rotation
 #define driveONEmmPerRota 35  // mm per 1 rotation // REQ CALIBRATION
-unsigned long driveONEstepsPerMM; // steps per 1 mm
-bool driveONECurrentDir;
 
+// -----INPUT----- 
 #define driveONEdistanceMM 20 // TO GO in mm
 double driveONESpeed = 1.0; // Speed in cm/sec
-double driveONEExpectedWorkTime; // how long to go in sec
 bool driveONEStartDir = true; // true = HIGH (right, out), false = LOW (left, in) // REQ CALIBRATION
-
+// ---------------
 
 
 double driveONETimePulsPercent = 0.5; //how long to send signal, float // REQ CALIBRATION
 double driveONETimePausePercent = 0.5; //pause before and after signal, float // REQ CALIBRATION
-double driveONETimePulsMS;
-double driveONETimePauseMS;
+
 int driveNum;
 int driveDirPin;
 int driveCurrentDir;
@@ -41,7 +38,7 @@ struct driveInfo GetDriveInfo(int driveNum){
    struct driveInfo _driveInfo;   
   if (driveNum == 1){
     _driveInfo.dirPin = driveONEDirPin;
-    _driveInfo.currentDir = driveONECurrentDir;
+    //_driveInfo.currentDir = 0;
     _driveInfo.startDir = driveONEStartDir;
     _driveInfo.stepPin = driveONEStepPin;
     
@@ -63,15 +60,6 @@ void setup() {
   pinMode(driveONEDirPin, OUTPUT);
   pinMode(driveONEStepPin, OUTPUT);
 
-  // Calculating dependant constants
-  // driveONEstepsPerMM = driveONEstepsPerRota / driveONEmmPerRota; // REQ CALIBRATION 45.71428!
-  // driveONEstepsTOgo = driveONEdistanceMM / driveONEstepsPerMM; // REQ CALIBRATION
-  
-  //driveONEExpectedWorkTimeMS = ((driveONEdistanceMM / 10) / driveONESpeed) * 1000; // MICRO SECONDS of work of driveOne
-  //driveONEExpectedStepDurationMS = driveONEstepsTOgo / driveONEExpectedWorkTimeMS;
-  //driveONETimePulsMS = driveONEExpectedStepDurationMS * driveONETimePulsPercent;
-  // driveONETimePauseMS = driveONEExpectedStepDurationMS * driveONETimePausePercent;
-  
   driveONEInfo = GetDriveInfo(1);
   SetDirection(driveONEInfo);
 }
