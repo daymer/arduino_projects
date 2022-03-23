65d8f9b18d412b0f8c636d9a574f433d22c5caa9
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2022-03-23 06:12:13 +0300

    Drive one test ver2

--- sketch_test_1/sketch_test_1.ino
+++ sketch_test_1/sketch_test_1.ino
@@ -1,6 +1,6 @@
 
 #define driveONEDirPin 0
-#define driveONEStepPin 1  
+#define driveONEStepPin 1
 
 #define driveONEstepsPerRota 1600 // steps per 1 rotation
 #define driveONEmmPerRota 35  // mm per 1 rotation // REQ CALIBRATION
@@ -10,15 +10,53 @@ bool driveONECurrentDir;
 #define driveONEdistanceMM 20 // TO GO in mm
 double driveONESpeed = 1.0; // Speed in cm/sec
 double driveONEExpectedWorkTime; // how long to go in sec
-bool driveONEDir = true; // true = HIGH (right, out), false = LOW (left, in) // REQ CALIBRATION
+bool driveONEStartDir = true; // true = HIGH (right, out), false = LOW (left, in) // REQ CALIBRATION
 
-unsigned long driveONEstepsTOgo;
-unsigned long driveONEExpectedWorkTimeMS;
-unsigned long driveONEExpectedStepDurationMS;
+
+
 double driveONETimePulsPercent = 0.5; //how long to send signal, float // REQ CALIBRATION
 double driveONETimePausePercent = 0.5; //pause before and after signal, float // REQ CALIBRATION
 double driveONETimePulsMS;
 double driveONETimePauseMS;
+int driveNum;
+int driveDirPin;
+int driveCurrentDir;
+
+struct driveInfo {
+  int dirPin;
+  int stepPin;
+  bool currentDir;
+  bool startDir;
+  unsigned long stepsPerMM;
+  unsigned long stepsTOgo;
+  unsigned long expectedWorkTimeMS;
+  unsigned long expectedStepDurationMS;
+  double timePulsMS;
+  double timePauseMS;
+  
+};
+
+struct driveInfo GetDriveInfo(int driveNum){
+   // Defining driveDirPin and driveCurrentDir
+   struct driveInfo _driveInfo;   
+  if (driveNum == 1){
+    _driveInfo.dirPin = driveONEDirPin;
+    _driveInfo.currentDir = driveONECurrentDir;
+    _driveInfo.startDir = driveONEStartDir;
+    _driveInfo.stepPin = driveONEStepPin;
+    
+    _driveInfo.stepsPerMM = driveONEstepsPerRota / driveONEmmPerRota; // REQ CALIBRATION 45.71428!
+    _driveInfo.stepsTOgo = driveONEdistanceMM / _driveInfo.stepsPerMM;
+    _driveInfo.expectedWorkTimeMS = ((driveONEdistanceMM / 10) / driveONESpeed) * 1000; // MICRO SECONDS of work of driveOne
+    _driveInfo.expectedStepDurationMS = _driveInfo.stepsTOgo / _driveInfo.expectedWorkTimeMS;
+    _driveInfo.timePulsMS = _driveInfo.expectedStepDurationMS * driveONETimePulsPercent;
+    _driveInfo.timePauseMS = _driveInfo.expectedStepDurationMS * driveONETimePausePercent;
+    
+    return _driveInfo;
+  }
+}
+
+struct driveInfo driveONEInfo;
  
 void setup() {
   // Setting pins as OUTPUT
@@ -26,39 +64,54 @@ void setup() {
   pinMode(driveONEStepPin, OUTPUT);
 
   // Calculating dependant constants
-  driveONEstepsPerMM = driveONEstepsPerRota / driveONEmmPerRota; // REQ CALIBRATION 45.71428!
-  driveONEstepsTOgo = driveONEdistanceMM / driveONEstepsPerMM; // REQ CALIBRATION
+  // driveONEstepsPerMM = driveONEstepsPerRota / driveONEmmPerRota; // REQ CALIBRATION 45.71428!
+  // driveONEstepsTOgo = driveONEdistanceMM / driveONEstepsPerMM; // REQ CALIBRATION
   
-  driveONEExpectedWorkTimeMS = ((driveONEdistanceMM / 10) / driveONESpeed) * 1000; // MICRO SECONDS of work of driveOne
-  driveONEExpectedStepDurationMS = driveONEstepsTOgo / driveONEExpectedWorkTimeMS;
-  driveONETimePulsMS = driveONEExpectedStepDurationMS * driveONETimePulsPercent;
-  driveONETimePauseMS = driveONEExpectedStepDurationMS * driveONETimePausePercent;
+  //driveONEExpectedWorkTimeMS = ((driveONEdistanceMM / 10) / driveONESpeed) * 1000; // MICRO SECONDS of work of driveOne
+  //driveONEExpectedStepDurationMS = driveONEstepsTOgo / driveONEExpectedWorkTimeMS;
+  //driveONETimePulsMS = driveONEExpectedStepDurationMS * driveONETimePulsPercent;
+  // driveONETimePauseMS = driveONEExpectedStepDurationMS * driveONETimePausePercent;
+  
+  driveONEInfo = GetDriveInfo(1);
+  SetDirection(driveONEInfo);
 }
 
 void loop() {
-  setDirection();
-  go();
+  
+  Go(1);
+  ChangeDirection(driveONEInfo);
 }
 
-void setDirection() {
-    // Defining direction of driveONE
-  if (driveONEDir == true) {
-    driveONECurrentDir = HIGH;}
+
+void SetDirection(struct driveInfo _driveInfo) {
+  if (_driveInfo.startDir == true) {
+    _driveInfo.currentDir = true;}
   else {
-    driveONECurrentDir = LOW;};
-  digitalWrite(driveONEDirPin, driveONECurrentDir);
+    _driveInfo.currentDir = false;};
+  digitalWrite(_driveInfo.dirPin, _driveInfo.currentDir);
 }
 
-void go(){
+void ChangeDirection(struct driveInfo _driveInfo) {
+  if (_driveInfo.currentDir == true) {
+    _driveInfo.currentDir = false;}
+  else {
+    _driveInfo.currentDir = true;};
+  digitalWrite(_driveInfo.dirPin, _driveInfo.currentDir);
+}
+
+void Go(int driveNum){
+  struct driveInfo _driveInfo;
+  _driveInfo = GetDriveInfo(driveNum);
+  
   unsigned int i;
 
-   for ( i = 0; i < driveONEstepsTOgo; i++ ){ 
-    digitalWrite(driveONEStepPin, LOW);
-    delay(driveONETimePauseMS / 2);
-    digitalWrite(driveONEStepPin, HIGH);
-    delay(driveONETimePulsMS);
-    digitalWrite(driveONEStepPin, LOW);
-    delay(driveONETimePauseMS / 2);
+   for ( i = 0; i < _driveInfo.stepsTOgo; i++ ){ 
+    digitalWrite(_driveInfo.stepPin, LOW);
+    delay(_driveInfo.timePauseMS / 2);
+    digitalWrite(_driveInfo.stepPin, HIGH);
+    delay(_driveInfo.timePulsMS);
+    digitalWrite(_driveInfo.stepPin, LOW);
+    delay(_driveInfo.timePauseMS / 2);
   }
   
 }
