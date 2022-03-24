7142b15ee235946a0aa49f3dd1e24a480756f986
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2022-03-25 02:37:57 +0300

    POC 2.0 - added 2 generation modes

--- sketch_test_1/sketch_test_1.ino
+++ sketch_test_1/sketch_test_1.ino
@@ -1,120 +1,171 @@
 
-#define driveONEDirPin 2
-#define driveONEStepPin 3
+#define driveONEDirPin 2 //0
+#define driveONEStepPin 3  //1
+unsigned int LoopCounter;
 
-#define driveONEstepsPerRota 1600 // steps per 1 rotation
-#define driveONEmmPerRota 35  // mm per 1 rotation // REQ CALIBRATION
+template <typename T>
+Print& operator<<(Print& printer, T value)
+{
+    printer.print(value);
+    return printer;
+}
+
+int driveONEstepsPerRota = 200; // steps per 1 rotation
+double driveONEmmPerRota = 3.0; // mm per 1 rotation // REQ CALIBRATION
 
 // -----INPUT----- 
-#define driveONEdistanceMM 200 // TO GO in mm
-double driveONESpeed = 1.0; // Speed in cm/sec
-bool driveONEStartDir = true; // true = HIGH (right, out), false = LOW (left, in) // REQ CALIBRATION
+double driveONEdistanceMM = 30.0; // TO GO in mm
+double driveONESpeed = 2.0; // Speed in mm/sec
+bool driveONEStartDir = false; // true = HIGH (left, in), false = LOW (right, out) // REQ CALIBRATION
 // ---------------
 
+// -----Modes-----
+#define PulsCalcMode 2
 
-double driveONETimePulsPercent = 0.5; //how long to send signal, float // REQ CALIBRATION
-double driveONETimePausePercent = 0.5; //pause before and after signal, float // REQ CALIBRATION
+// PulsCalcMode 1: calc puls\pause by percent
+double driveONETimePulsPercent = 0.4; //how long to send signal, float // REQ CALIBRATION
+double driveONETimePausePercent = 1 - driveONETimePulsPercent; //pause before and after signal, float // REQ CALIBRATION
+
+// PulsCalcMode 2: calc puls\pause time by fixed length
+int driveONEFixedPulsTimeMS = 5;
+// ---------------
 
 int driveNum;
-int driveDirPin;
-int driveCurrentDir;
 
 struct driveInfo {
   int dirPin;
   int stepPin;
   bool currentDir;
   bool startDir;
-  unsigned long stepsPerMM;
-  unsigned long stepsTOgo;
-  unsigned long expectedWorkTimeMS;
+  double stepsPerMM;
+  int stepsTOgo;
+  int expectedWorkTimeMS;
   unsigned long expectedStepDurationMS;
   double timePulsMS;
   double timePauseMS;
+  double selfcorrectionUS;
   
 };
 
+struct driveInfo driveOne;
+
 struct driveInfo GetDriveInfo(int driveNum){
    // Defining driveDirPin and driveCurrentDir
-   struct driveInfo _driveInfo;   
+  struct driveInfo _driveInfo;   
   if (driveNum == 1){
     _driveInfo.dirPin = driveONEDirPin;
     //_driveInfo.currentDir = 0;
     _driveInfo.startDir = driveONEStartDir;
     _driveInfo.stepPin = driveONEStepPin;
     
-    _driveInfo.stepsPerMM = driveONEstepsPerRota / driveONEmmPerRota; // REQ CALIBRATION 45.71428!
-    _driveInfo.stepsTOgo = driveONEdistanceMM / _driveInfo.stepsPerMM;
-    _driveInfo.expectedWorkTimeMS = ((driveONEdistanceMM / 10) / driveONESpeed) * 1000; // MICRO SECONDS of work of driveOne
-    _driveInfo.expectedStepDurationMS = _driveInfo.stepsTOgo / _driveInfo.expectedWorkTimeMS;
-    _driveInfo.timePulsMS = _driveInfo.expectedStepDurationMS * driveONETimePulsPercent;
-    _driveInfo.timePauseMS = _driveInfo.expectedStepDurationMS * driveONETimePausePercent;
+    _driveInfo.stepsPerMM = 1.0 / (driveONEmmPerRota / driveONEstepsPerRota); // REQ CALIBRATION 45.71428! #TODO: do smth with float
+    _driveInfo.stepsTOgo = driveONEdistanceMM * _driveInfo.stepsPerMM;
+    _driveInfo.expectedWorkTimeMS = (driveONEdistanceMM / driveONESpeed) * 1000; // MICRO SECONDS of work of driveOne
+    _driveInfo.expectedStepDurationMS = _driveInfo.expectedWorkTimeMS / _driveInfo.stepsTOgo;
+
+    if (PulsCalcMode == 1){
     
-    return _driveInfo;
+      _driveInfo.timePulsMS = _driveInfo.expectedStepDurationMS * driveONETimePulsPercent;
+      _driveInfo.selfcorrectionUS = (3.50 * 2) + 50.0;
+      _driveInfo.timePauseMS = (_driveInfo.expectedStepDurationMS * driveONETimePausePercent * 1000) - _driveInfo.selfcorrectionUS; 
+      }
+    else if (PulsCalcMode == 2){
+      _driveInfo.timePulsMS = driveONEFixedPulsTimeMS;
+      _driveInfo.selfcorrectionUS = (3.50 * 2) + 50.0;
+      _driveInfo.timePauseMS = ((_driveInfo.expectedStepDurationMS - _driveInfo.timePulsMS) * 1000) - _driveInfo.selfcorrectionUS; 
+    };
+      
+  return _driveInfo;
   }
 }
 
-struct driveInfo driveONEInfo;
- 
-void setup() {
-  // Setting pins as OUTPUT
-  pinMode(driveONEDirPin, OUTPUT);
-  pinMode(driveONEStepPin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  
-  driveONEInfo = GetDriveInfo(1);
-  SetDirection(driveONEInfo);
+struct driveInfo Go(struct driveInfo _driveInfo) {
+
+  // CHECK IF _driveInfo.timePulsMS > 5ms
+
+  if (_driveInfo.timePulsMS < 5){
+    while (1){
+      Serial << "Wrong rotation settings! timePuls < 5ms: " << _driveInfo.timePulsMS  << "\n";
+      blinkStart(200);
+    }
+  }
+
+  unsigned int i;
+  Serial << "GO: steps: " << _driveInfo.stepsTOgo << ", puls " << _driveInfo.timePulsMS << " ms, pause " << _driveInfo.timePauseMS << " us, selfcorrection: " << _driveInfo.selfcorrectionUS << " us" << "\n";
+  long int gt1 = millis();
+  for ( i = 0; i < _driveInfo.stepsTOgo; i++ ){ 
+    digitalWrite(_driveInfo.stepPin, HIGH);
+    // PORTD = PORTD | B01000000; 
+    delay(_driveInfo.timePulsMS);
+    digitalWrite(_driveInfo.stepPin, LOW);
+    //PORTD = PORTD & B01000000;
+    delayMicroseconds((_driveInfo.timePauseMS));
+  }
+  long int gt2 = millis();
+  double overtime = (gt2-gt1) - _driveInfo.expectedWorkTimeMS;
+  if (overtime != 0){
+     _driveInfo.selfcorrectionUS += (overtime/_driveInfo.stepsTOgo)*1000;
+    _driveInfo.timePauseMS -= (overtime/_driveInfo.stepsTOgo)*1000;   
+  }
+  Serial << "GO: expected: " << _driveInfo.expectedWorkTimeMS << " ms, actual: " <<  _driveInfo.expectedWorkTimeMS + overtime  << " ms" << "\n";
+  return _driveInfo;
 }
 
-void blinkStart(){
+void blinkStart(int t){
   unsigned int i;
 
    for ( i = 0; i < 5; i++ ){
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
+    delay(t);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+    delay(t);
    }
 }
 
-void loop() {
-  blinkStart();
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  Go(1);
-  digitalWrite(LED_BUILTIN, LOW);
-  ChangeDirection(driveONEInfo);
-}
-
-
-void SetDirection(struct driveInfo _driveInfo) {
+struct driveInfo SetDirection(struct driveInfo _driveInfo) {
   if (_driveInfo.startDir == true) {
     _driveInfo.currentDir = true;}
   else {
     _driveInfo.currentDir = false;};
   digitalWrite(_driveInfo.dirPin, _driveInfo.currentDir);
+  return _driveInfo;
 }
 
-void ChangeDirection(struct driveInfo _driveInfo) {
+struct driveInfo ChangeDirection(struct driveInfo _driveInfo) {
   if (_driveInfo.currentDir == true) {
     _driveInfo.currentDir = false;}
   else {
     _driveInfo.currentDir = true;};
   digitalWrite(_driveInfo.dirPin, _driveInfo.currentDir);
+  return _driveInfo;
 }
 
-void Go(int driveNum){
-  struct driveInfo _driveInfo;
-  _driveInfo = GetDriveInfo(driveNum);
+void setup() {
+  Serial.begin(9600);
+  // Setting pins as OUTPUT
+  pinMode(driveONEDirPin, OUTPUT);
+  pinMode(driveONEStepPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+   // Setting all Pboard ports to LOW
+  PORTD = B00000000;
   
-  unsigned int i;
+  driveOne = GetDriveInfo(1);
+  struct driveInfo driveONEInfo;
+  driveOne = SetDirection(driveOne);
+  Serial.println("Setup is done, starting the main programm");
+}
 
-   for ( i = 0; i < _driveInfo.stepsTOgo; i++ ){ 
-    digitalWrite(_driveInfo.stepPin, LOW);
-    delay(_driveInfo.timePauseMS / 2);
-    digitalWrite(_driveInfo.stepPin, HIGH);
-    delay(_driveInfo.timePulsMS);
-    digitalWrite(_driveInfo.stepPin, LOW);
-    delay(_driveInfo.timePauseMS / 2);
-  }
+
+void loop() {
+  LoopCounter++;
+  Serial << "Starting loop #" << LoopCounter << '\n';
+  blinkStart(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  long int t1 = millis();
+  driveOne = Go(driveOne);
+  digitalWrite(LED_BUILTIN, LOW);
+  driveOne = ChangeDirection(driveOne);
+  long int t2 = millis();
+  Serial.print("Loop took "); Serial.print(float(t2-t1)/1000); Serial<< " sec" << '\n';
   
 }
