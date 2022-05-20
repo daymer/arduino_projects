393516d8ddf687981b7fbf0ac33c3b488c1eff9e
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2022-05-20 20:59:29 +0300

    2 drives are working

--- AccelStepper_proto_1/AccelStepper_proto_1.ino
+++ AccelStepper_proto_1/AccelStepper_proto_1.ino
@@ -10,7 +10,7 @@
 // Alas its not possible to build an array of these with different pins for each :-(
 AccelStepper stepper1(AccelStepper::FULL2WIRE, 4, 5);
 AccelStepper stepper2(AccelStepper::FULL2WIRE, 6, 7);
-AccelStepper stepper3(AccelStepper::FULL2WIRE, 8, 9);
+//AccelStepper stepper3(AccelStepper::FULL2WIRE, 8, 9);
 
 // Up to 10 steppers can be handled as a group by MultiStepper
 MultiStepper steppers;
@@ -20,37 +20,42 @@ void setup() {
 
   // Configure each stepper
 
-  stepper1.setMaxSpeed(200.0); // maximum speed in steps per second
-  stepper1.setAcceleration(100.0);
-  stepper1.moveTo(24);
-  
-  stepper2.setMaxSpeed(300.0);
-  stepper2.setAcceleration(100.0);
-  stepper2.moveTo(1000000);
-  
-  stepper3.setMaxSpeed(300.0);
-  stepper3.setAcceleration(100.0);
-  stepper3.moveTo(1000000); 
+  stepper1.setMaxSpeed(3600.0); // maximum speed in steps per second
+  stepper1.setAcceleration(400.0);
+  //stepper1.moveTo(24);
+
+
+  stepper2.setMaxSpeed(32000);
+  stepper2.setAcceleration(8000.0);
+  //stepper2.moveTo(1000000);
+
+ 
+  //stepper3.setMaxSpeed(600.0);
+  //stepper3.setAcceleration(200.0);
+  //stepper3.moveTo(1000000); 
+
+
 
   // Then give them to MultiStepper to manage
   steppers.addStepper(stepper1);
   steppers.addStepper(stepper2);
-  steppers.addStepper(stepper3);
+  //steppers.addStepper(stepper3);
 }
 
 void loop() {
-  long positions[2]; // Array of desired stepper positions
+  delay(5000);
+  long positions[1]; // Array of desired stepper positions
   
-  positions[0] = 1000; // =17.5mm
-  positions[1] = 400;
+  positions[0] = -8000; // 200
+  positions[1] = 30000; // 400
   steppers.moveTo(positions);
   steppers.runSpeedToPosition(); // Blocks until all are in position
   delay(1000);
   
   // Move to a different coordinate
-  positions[0] = -100;
-  positions[1] = 100;
+  positions[0] = 8000;
+  positions[1] = -30000;
   steppers.moveTo(positions);
   steppers.runSpeedToPosition(); // Blocks until all are in position
-  delay(1000);
+  //delay(1000);
 }
